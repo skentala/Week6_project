eed6a444fe4886afbd023874d547392387a7aa4c
skentala1 <135027565+skentala@users.noreply.github.com>
2023-06-19 13:53:34 +0000

    Update 8 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,92 @@
 import "./styles.css";
+import { Chart } from "frappe-charts/dist/frappe-charts.min.esm";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const jsonQuery = {
+    "query": [
+        {
+            "code": "Vuosi",
+            "selection": {
+                "filter": "item",
+                "values": [
+                    "2000",
+                    "2001",
+                    "2002",
+                    "2003",
+                    "2004",
+                    "2005",
+                    "2006",
+                    "2007",
+                    "2008",
+                    "2009",
+                    "2010",
+                    "2011",
+                    "2012",
+                    "2013",
+                    "2014",
+                    "2015",
+                    "2016",
+                    "2017",
+                    "2018",
+                    "2019",
+                    "2020",
+                    "2021"
+                ]
+            }
+        },
+        {
+            "code": "Alue",
+            "selection": {
+                "filter": "item",
+                "values": [
+                    "SSS"
+                ]
+            }
+        },
+        {
+            "code": "Tiedot",
+            "selection": {
+                "filter": "item",
+                "values": [
+                    "vaesto"
+                ]
+            }
+        }
+    ],
+    "response": {
+        "format": "json-stat2"
+    }
+}
+
+
+
+async function showData () {
+  const url = "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px";
+  const res = await fetch(url, {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(jsonQuery)
+  });
+  if (!res.ok) return;
+  const data = await res.json();
+
+  const chartData = {
+      labels: Object.values(data.dimension.Vuosi.category.label),
+      datasets: [
+        {
+          name: "Population",
+          values: data.value
+        }
+      ]
+  }
+//  console.log(chartData);
+
+  const chart = new Chart("#chart", {
+        title: "",
+        data: chartData,
+        type: "line",
+        height: 450,
+        colors: ["#eb5146"]
+  });
+}
+
+showData();
